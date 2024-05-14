af9720dbb272b5e27c42bd2b9e9fcc001170d326
nuryamancode <iqbalnuryamann@gmail.com>
2024-05-14 11:20:26 +0700

    pengaduan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Masyarakat\PengaduanController::index');
-$routes->get('/test', 'Masyarakat\PengaduanController::penghapusan');
+$routes->get('/test', 'Masyarakat\PengaduanController::test');
 // $routes->get('/test/{text}', 'Masyarakat\PengaduanController::bm25');
 $routes->post('/store-pengaduan', 'Masyarakat\PengaduanController::store');
--- app/Controllers/Masyarakat/PengaduanController.php
+++ app/Controllers/Masyarakat/PengaduanController.php
@@ -29,15 +29,11 @@ class PengaduanController extends BaseController
 
     public function bm25($text)
     {
-        // token
         $token = $this->token_lower_clean($text);
-        // penghapusan
-        $penghapusan = $this->penghapusan($token);
-        // steamming
-        $steaming = $this->stemming($penghapusan);
-        // stopword
-        $stopword = $this->stopwords($steaming);
-        return $stopword;
+        $stopword = $this->stopwords($token);
+        $steaming = $this->stemming($stopword);
+        $penghapusan = $this->penghapusan($steaming);
+        return $penghapusan;
     }
 
 
@@ -85,50 +81,84 @@ class PengaduanController extends BaseController
     //     return $reportText;
     // }
 
+    //     public function test()
+    // {
+    //     $string = "nama budi alamat jalan pahlawan no 5 nomor telepon 089873612211 saudara heri pinjam uang besar janji kembali kurun 8 pinjam 7 maret 2019 sangkut sikap bayar hutang piutang kontak akses putus hubung saudara heri";
 
+    //     // Pola untuk menghapus informasi identifikasi
+    //     $patterns = [
+    //         '/\b(nama)\b\s+\w+/i',
+    //         '/\b(alamat)\b\s+(?:\w+\s*){1,5}/i',
+    //         '/\b(nomor telepon)\b\s+\d+/i',
+    //         '/\b(telepon)\b\s+\d+/i',
+    //         '/\b(alamat lengkap)\b\s+(?:\w+\s*){1,5}/i',
+    //         '/\b(jenis kelamin)\b\s+\w+/i',
+    //         '/\b(no ktp)\b\s+\w+/i',
+    //         '/\b(pekerjaan)\b\s+\w+/i'
+    //     ];
 
+    //     foreach ($patterns as $pattern) {
+    //         $string = preg_replace($pattern, '', $string);
+    //     }
+
+    //     // Menghapus spasi ganda dan memangkas spasi di awal dan akhir string
+    //     $hasil = preg_replace('/\s+/', ' ', trim($string));
+
+    //     echo "String awal: " . $string . "<br>";
+    //     echo "String setelah dihapus: " . $hasil;
+    // }
 
     public function token_lower_clean($text)
     {
-        // $text = 'Pada 2018, ada penipuan dari SMS setelah tunjangan hari raya (THR) cair. SMS tersebut mencantumkan tautan laman yang berisi informasi hadiah.';
-        $tokens = explode(" ", $text);
-        $tokens = preg_replace("/[^A-Za-z0-9]/", '', $tokens);
+        $tokens = preg_split('/\s+/', $text);
+        $tokens = array_map(function ($token) {
+            if (!empty($token)) {
+                return preg_replace("/[^A-Za-z0-9]/", '', $token);
+            }
+            return $token;
+        }, $tokens);
         $tokens = array_map('strtolower', $tokens);
+
+        $tokens = array_filter($tokens);
         return implode(" ", $tokens);
     }
 
+
+
     public function penghapusan($text)
     {
-        $patterns = array(
-            '/nama[\s:]+[\w\s]+/i',
-            '/jl[\s:]+[\w\s]+/i',
-            '/no[\s:]+[\d\s-]+/i',
-            '/[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/',
-        );
-        $cleaned_data = preg_replace($patterns, '', $text);
-        return $cleaned_data;
+        $patterns = [
+            '/\b(nama)\b\s+\w+/i',
+            '/\b(email)\b\s+\w+/i',
+            '/\b(alamat)\b\s+(?:\w+\s*){1,5}/i',
+            '/\b(nomor telepon)\b\s+\d+/i',
+            '/\b(telepon)\b\s+\d+/i',
+            '/\b(alamat lengkap)\b\s+(?:\w+\s*){1,5}/i',
+            '/\b(jenis kelamin)\b\s+\w+/i',
+            '/\b(no ktp)\b\s+\w+/i',
+            '/\b(pekerjaan)\b\s+\w+/i'
+        ];
+
+        foreach ($patterns as $pattern) {
+            $text = preg_replace($pattern, '', $text);
+        }
+
+        $hasil = preg_replace('/\s+/', ' ', trim($text));
+        return $hasil;
     }
 
     public function stemming($text)
     {
-        // Contoh teks
-        // $text = 'Pada 2018, ada penipuan dari SMS setelah tunjangan hari raya (THR) cair. SMS tersebut mengadukan tautan laman yang berisi informasi hadiah.';
-
-        // Inisialisasi StemmerFactory
         $stemmerFactory = new StemmerFactory();
         $stemmer = $stemmerFactory->createStemmer();
-
-        // Lakukan stemming pada teks
         $stemmedText = $stemmer->stem($text);
-
-        // Output teks setelah stemming
         return $stemmedText;
     }
     public function stopwords($text)
     {
-        // $text = 'Pada 2018, ada penipu/an dari SMS setelah tunjangan hari raya (THR) cair. SMS tersebut mencantumkan tautan laman yang berisi informasi hadiah.';
         $stopwords = new StopWord();
         $result = $stopwords->filterText($text);
-        return $result;
+        $cleanedText = preg_replace('/\s+/', ' ', $result);
+        return $cleanedText;
     }
 }
